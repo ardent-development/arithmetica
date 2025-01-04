55537ee22d81a2842669cf5667147af2e5e613d5
Akshat Singh <HackerDaGreat57@gmail.com>
2025-01-04 12:46:15 -0800

    Partially fix non-working arrow key escapes issue

--- src/ui.c
+++ src/ui.c
@@ -51,46 +51,42 @@ void do_ui() {
 			serial_ui.entry_len = 0; // entry length is always 0 on startup/new entry
 		}
 
-		key = stdio_getchar_timeout_us(100000);
+		key = stdio_getchar_timeout_us(0);
 		/*if(key != PICO_ERROR_TIMEOUT) {
 			printf("%d\n",key);
 		}*/
 
-		if(key > 50 && key < 127 && key != 27) { // a letter, number, symbol, etc.
+		if(key > 0 && key < 127 && key != 27) { // a letter, number, symbol, etc.
 			stdio_putchar_raw(key);
 			serial_ui.cursor_xpos++;
 			serial_ui.entry_len++;
 
-		} /*else if(key == 27) { // ESC: for escape keys like arrows, home/end, etc.
+		} else if(key == 27) { // ESC: for escape keys like arrows, home/end, etc.
+escape_key_detect:
 			int key_extend[3] = {0,0,0}; // max of 3 chars come after ESC.
 			for(uint8_t i = 0; i <= 2; ++i) { // get each of the 3 possible next chars
-				key_extend[i] = stdio_getchar_timeout_us(0);
+				key_extend[i] = stdio_getchar_timeout_us(5000);
 			}
-
 			// key_extend now contains int representations of each keycode of the
 			// special key.
 
 			if(key_extend[0] == 91 && key_extend[1] == 67) { // Right arrow
 				if(serial_ui.cursor_xpos < serial_ui.entry_len) {
-					//printf("\033[1C");
-					stdio_putchar_raw((int)'\033');
-					stdio_putchar_raw((int)'[');
-					//stdio_putchar_raw((int)'1');
-					stdio_putchar_raw((int)'C');
+					printf("\x1b[C");
 					serial_ui.cursor_xpos++;
 				}
 			} else if(key_extend[0] == 91 && key_extend[1] == 68) { // Left arrow
 				if(serial_ui.cursor_xpos > 0) {
-					//printf("\033[1D");
-					stdio_putchar_raw((int)'\033');
-					stdio_putchar_raw((int)'[');
-					//stdio_putchar_raw((int)'1');
-					stdio_putchar_raw((int)'D');
+					printf("\x1b[D");
 					serial_ui.cursor_xpos--;
 				}
 			}
 
-		}*/ else if(key == 127) { // Backspace
+			if(key_extend[2] == 27) { // Is there another escape key to process?
+				goto escape_key_detect;
+			}
+
+		} else if(key == 127) { // Backspace
 			if(serial_ui.cursor_xpos > 0) {
 				// Move back a char, print a space to make the character look empty, and
 				// move the cursor back again to produce functionality of a backspace.
@@ -98,10 +94,6 @@ void do_ui() {
 				serial_ui.cursor_xpos--;
 				serial_ui.entry_len--;
 			}
-		} else if(key == 49) {
-				printf("\033[1D");
-		} else if(key == 50) {
-				printf("\033[1C");
 		}
 	}
 }
